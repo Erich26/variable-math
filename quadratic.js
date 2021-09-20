f9c8cb82bf5daa2382c573853e0d1d41e47ad43a
Erich26 <erich.fredersdorff@codeimmersives.com>
2021-09-20 15:16:26 -0400

    minor spacing fix

--- quadratic.js
+++ quadratic.js
@@ -2,7 +2,7 @@ const a = 1;
 const b = 20;
 const c = 15;
 
-const root = Math.sqrt( (b * b) - (4 *a * c) );
+const root = Math.sqrt( (b * b) - (4 * a * c) );
 const den = 2 * a;
 
 const x1 = (-b + root)/den;
